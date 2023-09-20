86db5cfe4ed9aec1cdbf1e512bba21aceddb5765
rmmateosv <rmmateosv@gmail.com>
2023-09-20 11:18:31 +0200

    12009

--- Unidad1/src/fTexto/Modelo.java
+++ Unidad1/src/fTexto/Modelo.java
@@ -4,30 +4,45 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Modelo {
 	private String nombreFichero;
+	public SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Modelo(String nombreFichero) {
 		this.nombreFichero = nombreFichero;
 	}
 
-	public Alumno obtenerAlumno(String dni) {
+	public Alumno obtenerAlumno(String dniBuscado) {
 		// TODO Auto-generated method stub
 		Alumno resultado = null;
 		
 		//Declarar el fichero para leer
+		BufferedReader f = null;
 		try {
 			//Apertura del fichero
-			BufferedReader f = new BufferedReader(new FileReader(nombreFichero));
+			f = new BufferedReader(new FileReader(nombreFichero));
 			
 			//REcorrido del fichero 
 			String linea;
 			while((linea=f.readLine())!=null) {
-				
+				//Dividir la línea por ; y guardar en array de String
+				String[] campos = linea.split(";");
+				//Comprabamos si el dni de línea coincide con el dni buscado
+				if(dniBuscado.equalsIgnoreCase(campos[0])) {
+					//Alumno encontrado
+					resultado = new Alumno(campos[0],campos[1], 
+							formatoFecha.parse(campos[2]), Integer.parseInt(campos[3]), 
+							Float.parseFloat(campos[4]), Boolean.parseBoolean(campos[5]));
+					
+					//Acabar el método devolviendo el alumno
+					return resultado;
+				}
 			}
 			
-			//Cierre del fichero
+			
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -35,6 +50,17 @@ public class Modelo {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Se ha encontrado una fecha incorrecta en el fichero");
+		}
+		finally {
+			try {
+				f.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		return resultado;
--- Unidad1/src/fTexto/Principal.java
+++ Unidad1/src/fTexto/Principal.java
@@ -1,15 +1,18 @@
 package fTexto;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Principal {
-	public static Scanner t  = new Scanner(System.in);
-	//Declaramos el objeto modelo que accede a los datos
+	public static Scanner t = new Scanner(System.in);
+
+	// Declaramos el objeto modelo que accede a los datos
 	public static Modelo ad = new Modelo("alumnos.txt");
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int opcion=0;
+		int opcion = 0;
 		do {
 			System.out.println("Selecciona una opción");
 			System.out.println("0-Salir");
@@ -18,40 +21,57 @@ public class Principal {
 			System.out.println("3-Modificar Alumno");
 			System.out.println("4-Borrar Alumno");
 			System.out.println("5-Mostrar por dni");
-			opcion = t.nextInt();t.nextLine();
-			
-			switch(opcion) {
-				case 1:
-					altaAlumno();
-					break;
-				case 2:
-			
-					
-					break;
-				case 3:
-				
-					break;
-				case 4:
-			
-					break;
-				case 5:
-				
-					break;
-				
-				
+			opcion = t.nextInt();
+			t.nextLine();
+
+			switch (opcion) {
+			case 1:
+				altaAlumno();
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+
 			}
-		
-		}while(opcion!=0);
+
+		} while (opcion != 0);
 	}
+
 	private static void altaAlumno() {
 		// TODO Auto-generated method stub
-		//Pedir solamente el dni
-		System.out.println("DNI:");
-		String dni = t.nextLine();
-		//Comprobar si existe el dni en el fichero
-		Alumno a = ad.obtenerAlumno(dni);
-		
-		
+		try {
+			// Pedir solamente el dni
+			System.out.println("DNI:");
+			String dni = t.nextLine();
+			// Comprobar si existe el dni en el fichero
+			Alumno a = ad.obtenerAlumno(dni);
+			if (a == null) {
+				// Alumno no existes, por lo que se puede crear
+				a= new Alumno();
+				// Pedir resto de datos
+				System.out.println("Nombre:");
+				a.setNombre(t.nextLine());
+				System.out.println("Fecha Nacimiento (dd/mm/yyyy)");
+
+				a.setFechaN(ad.formatoFecha.parse(t.nextLine()));
+
+			} else {
+				System.out.println("Error, el alumno ya existe");
+			}
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error, fecha incorrecta");
+		}
 	}
 
 }
