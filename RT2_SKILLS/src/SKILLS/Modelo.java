4eb1c84687f7e723a3d2868890bc46d44c752aa1
rmmateosv <rmmateosv@gmail.com>
2024-04-30 14:26:25 +0200

    0430

--- RT2_SKILLS/src/SKILLS/Modelo.java
+++ RT2_SKILLS/src/SKILLS/Modelo.java
@@ -1,11 +1,14 @@
 package SKILLS;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 
 public class Modelo {
@@ -14,7 +17,7 @@ public class Modelo {
 	private final String URL = "jdbc:mysql://localhost:3307/skills";
 	private final String US = "root";
 	private final String PS = "root";
-	
+
 	public Modelo() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -36,7 +39,7 @@ public class Modelo {
 	public void setConexion(Connection conexion) {
 		this.conexion = conexion;
 	}
-	
+
 	public void cerrar() {
 		if (conexion != null) {
 			try {
@@ -49,50 +52,137 @@ public class Modelo {
 	}
 
 	public Alumno obtenerAlumno(String dni) {
-		
+
 		Alumno resultado = null;
-		
+
 		try {
 			PreparedStatement ps = conexion.prepareStatement("select * from alumno as al inner join modalidad as mo "
-					+ "on al.modalidad = mo.id "
-					+ "where dni = ?");
+					+ "on al.modalidad = mo.id " + "where dni = ?");
 			ps.setString(1, dni);
 			ResultSet rs = ps.executeQuery();
-			
-			if(rs.next()) {
-				resultado = new Alumno(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getBoolean(5),new Modalidades(rs.getInt(3),rs.getString(7)));
+
+			if (rs.next()) {
+				resultado = new Alumno(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(5), rs.getBoolean(6),
+						new Modalidades(rs.getInt(4), rs.getString(8)));
 			}
-			
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return resultado;
 	}
 
 	public ArrayList<Modalidades> obtenerModalidades() {
-		
+
 		ArrayList<Modalidades> resultado = new ArrayList();
-		
+
 		try {
 			Statement st = conexion.createStatement();
 			ResultSet datos = st.executeQuery("select * from modalidad");
-			
-			while(datos.next()){
+
+			while (datos.next()) {
 				resultado.add(new Modalidades(datos.getInt(1), datos.getString(2)));
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return resultado;
 	}
-	
-	
-	
+
+	public boolean crearModalidad(Modalidades modalidad) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		try {
+			CallableStatement CB = conexion.prepareCall("{? = call crearModalidad(?)}");
+			CB.registerOutParameter(1, Types.INTEGER);
+			CB.setString(2, modalidad.getModalidad());
+			CB.executeUpdate();
+			modalidad.setId(CB.getInt(1));
+			resultado = true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public Modalidades obtenerModalidad(int opc) {
+		// TODO Auto-generated method stub
+		Modalidades resultado = null;
+		try {
+			PreparedStatement ps = conexion.prepareStatement("Select * from modalidad where id = ?");
+			ps.setInt(1, opc);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				resultado = new Modalidades(rs.getInt(1), rs.getString(2));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return resultado;
+	}
+
+	public boolean crearAlumno(Alumno alumno) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		try {
+			PreparedStatement ps = conexion.prepareStatement("Insert into alumno values(default, ?, ?, ?, 0, false)");
+			ps.setString(1, alumno.getDni());
+			ps.setString(2, alumno.getNombre());
+			ps.setInt(3, alumno.getModalidad().getId());
+			int numfilas = ps.executeUpdate();
+			if (numfilas == 1) {
+				resultado = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public boolean crearPrueba(Prueba prueba) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		try {
+			PreparedStatement ps = conexion.prepareStatement("Insert into prueba values(default, ?, ? ,? ,?)");
+			ps.setInt(1, prueba.getModalidad());
+			ps.setDate(2, new Date(prueba.getFecha().getTime()));
+			ps.setString(3, prueba.getDescripcion());
+			ps.setInt(4, prueba.getPuntuacion());
+			int numfilas = ps.executeUpdate();
+			if (numfilas == 1) {
+				resultado = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
+	public ArrayList<Prueba> obtenerPruebas(int id) {
+		// TODO Auto-generated method stub
+		ArrayList<Prueba> resultado = new ArrayList();
+		try {
+			PreparedStatement ps = conexion.prepareStatement("Select * from prueba where modalidad = ?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				resultado.add(new Prueba(rs.getInt(1), rs.getInt(2), rs.getDate(3), rs.getString(4), rs.getInt(5)));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+
 }
--- RT2_SKILLS/src/SKILLS/Principal.java
+++ RT2_SKILLS/src/SKILLS/Principal.java
@@ -1,5 +1,7 @@
 package SKILLS;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,9 +9,9 @@ public class Principal {
 
 	public static Scanner tec = new Scanner(System.in);
 	public static Modelo modelo = new Modelo();
-	
+
 	public static void main(String[] args) {
-		
+
 		int opc = -1;
 
 		if (modelo.getConexion() != null) {
@@ -30,14 +32,14 @@ public class Principal {
 					ejercicio1();
 					break;
 				case 2:
-					//ejercicio2();
+					ejercicio2();
 					break;
 				case 3:
-					//ejercicio3();
+					// ejercicio3();
 					;
 					break;
 				case 4:
-					//ejercicio4();
+					// ejercicio4();
 					break;
 				}
 			} while (opc != 0);
@@ -46,39 +48,101 @@ public class Principal {
 
 	}
 
+	private static void ejercicio2() {
+		// TODO Auto-generated method stub
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("ddMMYY");
+			mostrarModalidades();
+			System.out.println("Introduce un código de modalidad");
+			Modalidades modalidad = modelo.obtenerModalidad(tec.nextInt());
+			tec.nextLine();
+			if (modalidad == null) {
+				System.err.println("No se ha encontrado la modalidad");
+			} else {
+
+				Prueba prueba = new Prueba();
+				prueba.setModalidad(modalidad.getId());
+				System.out.println("Introduce la fecha de la Modalidad");
+				prueba.setFecha(sdf.parse(tec.nextLine()));
+				System.out.println("Introduce la descripción de la prueba");
+				prueba.setDescripcion(tec.nextLine());
+				System.out.println("Introduce la puntuación de la prueba");
+				prueba.setPuntuacion(tec.nextInt());tec.nextLine();
+				if (modelo.crearPrueba(prueba)) {
+					ArrayList<Prueba> pruebas = modelo.obtenerPruebas(modalidad.getId());
+					int total = 0;
+					for (Prueba p : pruebas) {
+						System.out.println(p);
+						total += p.getPuntuacion();
+					}
+					System.out.println("La puntutación total es: " +total);
+				} else {
+					System.err.println("No se ha podido crear la prueba");
+				}
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	private static void ejercicio1() {
-		
+
 		System.out.println("Introduce el dni del alumno: ");
 		String dni = tec.nextLine();
-		
+
 		Alumno alumno = modelo.obtenerAlumno(dni);
-		if(alumno != null) {
-			System.out.println("ALUMNO YA EXISTENTE en la modalidad " +alumno.getModalidad().getModalidad());
-		}
-		else {
+		if (alumno != null) {
+			System.out.println("ALUMNO YA EXISTENTE en la modalidad " + alumno.getModalidad().getModalidad());
+		} else {
 			mostrarModalidades();
 			System.out.println("Introduce el id de la modalidad (0 para crear una nueva): ");
-			int opc = tec.nextInt(); tec.nextLine();
-			if(opc == 0) {
+			int opc = tec.nextInt();
+			tec.nextLine();
+			Modalidades modalidad = null;
+			boolean error = false;
+			if (opc == 0) {
 				// Crear modalidad
-				
+				modalidad = new Modalidades();
+				System.out.println("Introduce la descripción de la modalidad");
+				modalidad.setModalidad(tec.nextLine());
+
+				if (modelo.crearModalidad(modalidad)) {
+					System.out.println("Modalidad " + modalidad.getId() + " Creada con éxito");
+
+				} else {
+					System.err.println("Se ha producido un error al crear la modalidad");
+					error = true;
+				}
+
+			} else {
+				// comprobar que existe la modalidad
+				modalidad = modelo.obtenerModalidad(opc);
+				if (modalidad == null) {
+					System.err.println("Modalidad no encontrada");
+					error = true;
+				}
 			}
-			else {
-				//comprobar que existe la modalidad
+			if (!error) {
+				System.out.println("Introduce el nombre del alumno");
+				alumno = new Alumno(0, dni, tec.nextLine(), 0, false, modalidad);
+				if (!modelo.crearAlumno(alumno)) {
+					System.err.println("El alumno no ha podido ser creado");
+				}
+
 			}
-			// Crear alumno
-			
+
 		}
-		
+
 	}
 
 	private static void mostrarModalidades() {
 		ArrayList<Modalidades> listaMoladidades = modelo.obtenerModalidades();
-		
+
 		for (Modalidades modalidades : listaMoladidades) {
 			System.out.println(modalidades);
 		}
-		
+
 	}
 
 }
